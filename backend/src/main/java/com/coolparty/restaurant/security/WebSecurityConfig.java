389542c9e7b15e05dcc61e6548ab751347683680
igorpapr <igorpapr@gmail.com>
2021-02-19 14:38:56 +0200

    temporarily commented spring security

--- backend/src/main/java/com/coolparty/restaurant/security/WebSecurityConfig.java
+++ backend/src/main/java/com/coolparty/restaurant/security/WebSecurityConfig.java
@@ -34,14 +34,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic()
-                .and()
-                .csrf().disable();
+//        http
+//                //.authorizeRequests()
+//                //.antMatchers("/").permitAll()
+//                //.anyRequest().authenticated()
+//                //.and()
+//                .httpBasic()
+//                .and()
+//                .csrf().disable();
 
     }
 
